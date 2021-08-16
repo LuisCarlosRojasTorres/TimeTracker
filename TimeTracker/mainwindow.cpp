de7653aabd1f65025687d4cdd5e7b201f49ed77d
Luis Carlos Absalón Rojas Torres <redtower.soft@gmail.com>
2021-08-15 23:07:36 -0300

    DlgAbout
    Foi adicionado o Dialog DlgAbout com informacao de contato

--- TimeTracker/mainwindow.cpp
+++ TimeTracker/mainwindow.cpp
@@ -6,6 +6,16 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->cB_ARN->addItem(QString("Perceptron"));
+    ui->cB_ARN->addItem(QString("Adaline"));
+    ui->cB_ARN->addItem(QString("Adaline"));
+
+
+    setWindowTitle(QString("TimeTracker" + version));
+    showMaximized();
+
+    connect(ui->actionAbout, &QAction::triggered, about, &DlgAbout::exec);
 }
 
 MainWindow::~MainWindow()
